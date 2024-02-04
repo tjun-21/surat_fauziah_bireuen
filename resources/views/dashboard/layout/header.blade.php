73982ff790ea63780931795cd50bf86bc9eb626f
Nabila Mufida <nabilamuf@MacBook-Pro-Nabila.local>
2024-02-04 23:48:24 +0700

    ubah side bar

--- resources/views/dashboard/layout/header.blade.php
+++ resources/views/dashboard/layout/header.blade.php
@@ -1,12 +1,46 @@
-<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">RS Fauziah Bireun</a>
-    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-    <div class="navbar-nav">
-      <div class="nav-item text-nowrap">
-        <a class="nav-link px-3" href="#">Logout</a>
+<header class="navbar sticky-top bg-dark p-0" >
+  <nav class="navbar navbar-expand-lg navbar-dark ">
+    <div class="container">
+      <a class="navbar-brand" href="#">RSUD. FAUZIAH BIREUEN</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+         
+        </ul>
+        <ul class="navbar-nav"> 
+
+          @auth
+           <li class="nav-item dropdown">
+         
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              
+              Selamat Datang, {{ auth()-> user()->name }}
+            </a>
+         
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="/dashboard"> <i class="bi bi-layout-text-sidebar-reverse"></i> My Dashbord</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li>
+                <form action="/logout" method="post">
+                    @csrf
+                    <button type="submit" class="dropdown-item"> <i class="bi bi-box-arrow-right"></i> Logout </button>
+                </form>
+            
+              </li>
+            </ul>
+          </li>
+
+          @else   
+          <li class="nav-item">
+            <a href="/login" class="nav-link {{ ($active ===  "login")? 'active':'' }}"> <i class="bi bi-box-arrow-in-right"></i>
+              Login </a>
+          </li>
+
+          @endauth
+        </ul>
       </div>
     </div>
+  </nav>
   </header>
